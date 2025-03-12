6910a90eaf2f0892a11f279c4bc72c9088f2d0f9
Matt Secrest <secrmatt@gmail.com>
2025-03-12 04:08:22 -0700

    raisins ingredients random

--- content.js
+++ content.js
@@ -6,21 +6,24 @@ function addRaisinsToIngredients() {
     if (ingredientsList) {
       const newIngredient = document.createElement("li");
       newIngredient.classList.add("mm-recipes-structured-ingredients__list-item");
+  
+      const quantities = ["1 cup", "a handful of", "a few", "2 tablespoons", "a sprinkle of", "a dash of"];
+      const randomQuantity = quantities[Math.floor(Math.random() * quantities.length)];
+  
       newIngredient.innerHTML = `
         <p>
-          <span data-ingredient-quantity="true">1</span>
-          <span data-ingredient-unit="true">cup</span>
+          <span data-ingredient-quantity="true">${randomQuantity}</span>
           <span data-ingredient-name="true">raisins</span>
         </p>
       `;
   
       const ingredientItems = ingredientsList.querySelectorAll(".mm-recipes-structured-ingredients__list-item");
-      const randomIndex = Math.floor(Math.random() * ingredientItems.length); // Get a random index
+      const randomIndex = Math.floor(Math.random() * ingredientItems.length);
   
-      if (randomIndex === 0) { // If it's the first item, append
+      if (randomIndex === 0) {
         ingredientsList.prepend(newIngredient);
       } else {
-        ingredientsList.insertBefore(newIngredient, ingredientItems[randomIndex]); // Insert before the random item
+        ingredientsList.insertBefore(newIngredient, ingredientItems[randomIndex]);
       }
     }
   }
